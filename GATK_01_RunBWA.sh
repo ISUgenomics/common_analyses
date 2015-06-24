5721cb2c6775bd3cdb2934adddf89b30fecc175f
Arun Seetharam <arnstrm@iastate.edu>
2015-06-24 10:21:37 -0500

    chmod

--- GATK_01_RunBWA.sh
+++ GATK_01_RunBWA.sh
@@ -4,11 +4,11 @@
 module load picard_tools
 module load java
 module load bwa
+module load samtools
 
 READ1="$1"
 READ2="$2"
 OUTNAME=$(basename ${READ1%.*} | cut -f 1-3 -d "_")
 REF="/home/arnstrm/arnstrm/20150413_Graham_SoybeanFST/01_DATA/B_REF/Gmax_275_v2.0.fa"
-
-bwa mem -M -t 16 -p ${REF} ${READS} > ${OUTNAME}.sam
+bwa mem -M -t 16 -p ${REF} ${READS} | samtools view -buS -o ${OUTNAME}.bam
 
