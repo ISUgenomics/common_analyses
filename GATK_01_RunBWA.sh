e5c9a8478af7d309da757e6d9b46653744d34cee
isugif <severin@iastate.edu>
2015-06-24 11:47:43 -0500

    minor fix of READS variable

--- GATK_01_RunBWA.sh
+++ GATK_01_RunBWA.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # perfomrs mapping of reads to the indexed reference genome
 # uses the options specified in "best practices"
-module load picard_tools
+#module load picard_tools
+module load picard-tools
 module load java
 module load bwa
 module load samtools
 
-READ1="$1"
-READ2="$2"
+READS="$1 $2"
+#READ1="$1"
+#READ2="$2"
 REF="$3"
 OUTNAME=$(basename ${READ1%.*} | cut -f 1-3 -d "_")
 #REF="/home/arnstrm/arnstrm/20150413_Graham_SoybeanFST/01_DATA/B_REF/Gmax_275_v2.0.fa"
