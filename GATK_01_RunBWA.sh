0fdb665631c1ff8cb652e30c890248915e886115
Arun Seetharam <arnstrm@iastate.edu>
2015-06-25 12:03:42 -0500

    modified to consider single-end reads as well

--- GATK_01_RunBWA.sh
+++ GATK_01_RunBWA.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # perfomrs mapping of reads to the indexed reference genome
 # uses the options specified in "best practices"
-#module load picard_tools
-module load picard-tools
+module load picard_tools
 module load java
 module load bwa
 module load samtools
 
-READ1="$1"
-READ2="$2"
-READS="$READ1 $READ2"
-REF="$3"
-OUTNAME=$(basename ${READ1%.*} | cut -f 1-3 -d "_")
-#REF="/home/arnstrm/arnstrm/20150413_Graham_SoybeanFST/01_DATA/B_REF/Gmax_275_v2.0.fa"
-bwa mem -M -t 16 -p ${REF} ${READS} | samtools view -buS - > ${OUTNAME}.bam
+REF="/data003/GIF/genomes/sequences/TIL01/TIL01_MaSuRCA_scaffolds.fasta"
+THREADS="8"
 
+if [ "$#" -eq 2 ]; then
+  READ1="$1"
+  READ2="$2"
+  OUTNAME=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
+  bwa mem -M -t ${THREADS} ${REF} -p ${READ1} ${READ2} | samtools view -buS - > ${OUTNAME}.bam
+elif [ "$#" -eq 1 ]; then
+  READ1="$1"
+  OUTNAME=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
+  bwa mem -M -t ${THREADS} ${REF} ${READ1} | samtools view -buS - > ${OUTNAME}.bam
+else
+  echo "ERROR: INVALID NUMBER OF ARGUMENTS"
+fi
