0b6873f9cbcceda749487cd0a00c9c956f7356e9
isugif <severin@iastate.edu>
2015-06-26 09:56:16 -0500

    added python command

--- GATK_00_PrepareRef.sh
+++ GATK_00_PrepareRef.sh
@@ -18,7 +18,7 @@ samtools faidx ${REF}
 bwa index -a bwtsw ${REF}
 FIL
 # Create interval list (here 100 kb intervals)
-fasta_length.py ${REF} > ${REF%.*}_length.txt
+python fasta_length.py ${REF} > ${REF%.*}_length.txt
 bedtools makewindows -w 100000 -g ${REF%.*}_length.txt > ${REF%.*}_100kb_coords.bed
 java -Xmx100G -jar $PICARD/picard.jar BedToIntervalList \
   INPUT=${REF%.*}_100kb_coords.bed \
