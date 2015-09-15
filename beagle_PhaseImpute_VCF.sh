9a6d1de6b088f9b7c497327c3535c118509121a0
Andrew Severin <severin@iastate.edu>
2015-09-15 10:28:38 -0500

    added diAllelic prefilter of input file

--- beagle_PhaseImpute_VCF.sh
+++ beagle_PhaseImpute_VCF.sh
@@ -7,10 +7,12 @@
 INPUT="$1"
 OUTPUT=$(basename ${INPUT%.*})
 
+awk '(/#/ || ($4!~"," && $5!~","))' ${INPUT} > ${OUTPUT}_diAllelic.vcf
 module load java/1.7.0_76
 module load beagle
 
-beagle="/data003/GIF/software/packages/beagle/r1399/beagle.jar"
+#put this in the module above
+#beagle="/data003/GIF/software/packages/beagle/r1399/beagle.jar"
 
 java -Xmx100g -jar ${beagle} \
  nthreads=16 \
@@ -18,5 +20,5 @@ java -Xmx100g -jar ${beagle} \
  phase-its=40 \
  impute-its=10 \
  ibd=true \
- gtgl=${INPUT} \
+ gtgl=${OUTPUT}_diAllelic.vcf \
  out=${OUTPUT}_PhasedImputed.vcf
