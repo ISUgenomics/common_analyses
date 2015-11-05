575809aea51ae0e06d8e7d3098e1d49f5ed06372
Andrew Severin <severin@iastate.edu>
2015-11-05 10:03:44 -0600

    added cleanup

--- SOYGO/SoyGO.sh
+++ SOYGO/SoyGO.sh
@@ -6,3 +6,5 @@ perl $SOYGO/SOYGO.pl $SOYGO/Gmv2_GODb $1
 R CMD BATCH "--args $1" $SOYGO/Script_Fisher.R
 perl $SOYGO/combinefilesbyGO.pl $1_BP_fisher.txt $SOYGO/ATH_GO_GOSLIM.022714  $1_BP_output.txt  > $1.BP.final
 perl $SOYGO/combinefilesbyGO.pl $1_MF_fisher.txt $SOYGO/ATH_GO_GOSLIM.022714  $1_MF_output.txt  > $1.MF.final
+rm $1.list*
+rm $1.txt
