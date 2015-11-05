62d9d1aeb30216695d717530e5e371e7e938b4e6
Andrew Severin <severin@iastate.edu>
2015-11-05 07:58:08 -0600

    added SOYGO variable for directory

--- SOYGO/SoyGO.sh
+++ SOYGO/SoyGO.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-perl GOinfoKevinupdate2.pl $1 Gmv2_GODb > $1.GOIDs
-perl SOYGO.pl Gmv2_GODb $1
-R CMD BATCH "--args $1" Script_Fisher.R
-perl combinefilesbyGO.pl $1_BP_fisher.txt ATH_GO_GOSLIM.022714  $1_BP_output.txt  > $1.BP.final
-perl combinefilesbyGO.pl $1_MF_fisher.txt ATH_GO_GOSLIM.022714  $1_MF_output.txt  > $1.MF.final
+export SOYGO="/data005/GIF2/severin/isugif/common_analyses/SOYGO"
+perl $SOYGO/GOinfoKevinupdate2.pl $1 $SOYGO/Gmv2_GODb > $1.GOIDs
+perl $SOYGO/SOYGO.pl $SOYGO/Gmv2_GODb $1
+R CMD BATCH "--args $1" $SOYGO/Script_Fisher.R
+perl $SOYGO/combinefilesbyGO.pl $1_BP_fisher.txt $SOYGO/ATH_GO_GOSLIM.022714  $1_BP_output.txt  > $1.BP.final
+perl $SOYGO/combinefilesbyGO.pl $1_MF_fisher.txt $SOYGO/ATH_GO_GOSLIM.022714  $1_MF_output.txt  > $1.MF.final
