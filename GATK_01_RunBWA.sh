4b8766c39f95f5b34a9dbcd02234b4c800f0cd24
Andrew Severin <severin@iastate.edu>
2015-11-09 09:49:31 -0600

    added functionality with genome modules

--- GATK_01_RunBWA.sh
+++ GATK_01_RunBWA.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 # perfomrs mapping of reads to the indexed reference genome
 # uses the options specified in "best practices"
+
+#command genomeModule READ1 READ2
+
 module load picard_tools
 module load java
-module load bwa
+module load bwa/0.7.12b
 module load samtools
+module load $1
 
-REF="/data003/GIF/genomes/sequences/TIL01/TIL01_MaSuRCA_scaffolds.fasta"
+REF="$GENOMEDIR/$GNAME"
 # this option might be the frequetly changed, hence not it's a variable
 THREADS="8"
 # if the reads are paired then use -p option
-if [ "$#" -eq 2 ]; then
-  READ1="$1"
-  READ2="$2"
+if [ "$#" -eq 3 ]; then
+  READ1="$2"
+  READ2="$3"
   OUTNAME=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
-  bwa mem -M -t ${THREADS} ${REF} -p ${READ1} ${READ2} | samtools view -buS - > ${OUTNAME}.bam
+  bwa mem -M -t ${THREADS} ${REF} ${READ1} ${READ2} | samtools view -buS - > ${OUTNAME}.bam
 # if not just use the reads as single reads
 elif [ "$#" -eq 1 ]; then
-  READ1="$1"
+  READ1="$2"
   OUTNAME=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
   bwa mem -M -t ${THREADS} ${REF} ${READ1} | samtools view -buS - > ${OUTNAME}.bam
 # if number of arguments do not match, raise error
 else
   echo "ERROR: INVALID NUMBER OF ARGUMENTS"
+  echo "GATK_01_RunBWA.sh genomeModule READ1 READ2"
 fi
