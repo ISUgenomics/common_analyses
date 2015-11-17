39d289917eac5495f3416a1bacfb231d64c31c1d
Andrew Severin <severin@iastate.edu>
2015-11-17 11:42:14 -0600

    this script generates the required gatk commands parsed by 10kb intervals

---
+++ GATK_04_combinedRun.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+module load $1
+
+#Grab bamfiles that will be used for input. all bam files in the folder will be selected.
+#these files will be written to a temp file that will be read in later to create the input line for each command
+unset -v bamfiles
+bamfiles=(*.bam)
+for bam in ${bamfiles[@]}; do \
+echo -en "-I ${bam} "; \
+done > temp
+
+#combine the reference genome, 100k genomic intervals and the input files into gatk commands
+#need to figure out how to include direct path to genomeanalysistk.jar file if that is necessary
+while read line; do \
+g2=$(echo $line | awk '{print $1":"$2"-"$3}'); \
+g1=$(echo $line | awk '{print $1"_"$2"_"$3}'); \
+
+echo -n "java -Xmx16g -Djava.io.tmpdir=\${TMPDIR} -jar /data003/GIF/software/packages/gatk/3.3/GenomeAnalysisTK.jar -T HaplotypeCaller --pcr_indel_model NONE \
+-R ${GENOMEFASTA} \
+$(cat temp) \
+-L "${g2}" --genotyping_mode DISCOVERY -stand_emit_conf 10 -stand_call_conf 30 -o \${TMPDIR}/"${g1}".vcf;"; \
+echo "mv \${TMPDIR}/"${g1}".vcf \$PBS_O_WORKDIR/" ; \
+done<${GENOMEINTERVALS}  > gatk.cmds
+
+cat <<-FIL > GATK04test.sub
+#!/bin/bash
+#PBS -q batch
+#PBS -l nodes=8:ppn=16
+#PBS -l walltime=48:00:00
+#PBS -N GATK05
+#PBS -o \${PBS_JOBNAME}.o\${PBS_JOBID} -e \${PBS_JOBNAME}.e\${PBS_JOBID}
+#PBS -m ae -M andrewseverin@gmail.com
+cd \$PBS_O_WORKDIR
+ulimit -s unlimited
+chmod g+rw \${PBS_JOBNAME}.[eo]\${PBS_JOBID}
+module use /data003/GIF/software/modules
+module load parallel
+module load java/1.7.0_76
+which java
+parallel --jobs 8 --sshloginfile \$PBS_NODEFILE \
+  --joblog gatk_progress_05.log --workdir \$PWD < gatk.cmds
+ssh condo "qstat -f \${PBS_JOBID} |head"
+FIL
