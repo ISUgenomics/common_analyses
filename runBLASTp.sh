c71e125a045b7c1aaed4c34114badec3ec4a4a13
Andrew Severin <severin@iastate.edu>
2016-03-15 08:06:19 -0500

    added qcovs to calculate query coverage

--- runBLASTp.sh
+++ runBLASTp.sh
@@ -8,7 +8,7 @@ blastp \
  -query "${infile}" \
  -db "${database}" \
  -out "${outfile}" \
- -evalue 1e-20 \
+ -evalue 1e-3 \
  -num_threads 1 \
  -max_target_seqs 1 \
- -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore staxids salltitles"
+ -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore staxids salltitles qcovs"
