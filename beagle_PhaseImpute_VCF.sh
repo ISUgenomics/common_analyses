96bdbba1cc4e6410859b9d825504d85c7184cddf
Andrew Severin <severin@iastate.edu>
2016-05-24 08:02:01 -0500

    removed some comments

--- beagle_PhaseImpute_VCF.sh
+++ beagle_PhaseImpute_VCF.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # performs phasing and imputation of a VCF file
 # using Beagle
 # run the script as :
@@ -7,12 +8,10 @@
 INPUT="$1"
 OUTPUT=$(basename ${INPUT%.*})
 
-awk '(/#/ || ($4!~"," && $5!~","))' ${INPUT} > ${OUTPUT}_diAllelic.vcf
+awk '(/#/ || ($4!~"," && $5!~","))' ${INPUT} > ${OUTPUT}_biAllelic.vcf
 module load java/1.7.0_76
 module load beagle
 
-#put this in the module above
-#beagle="/data003/GIF/software/packages/beagle/r1399/beagle.jar"
 
 java -Xmx100g -jar ${beagle} \
  nthreads=16 \
@@ -20,5 +19,6 @@ java -Xmx100g -jar ${beagle} \
  phase-its=40 \
  impute-its=10 \
  ibd=true \
- gtgl=${OUTPUT}_diAllelic.vcf \
+ gtgl=${OUTPUT}_biAllelic.vcf \
  out=${OUTPUT}_PhasedImputed.vcf
+
