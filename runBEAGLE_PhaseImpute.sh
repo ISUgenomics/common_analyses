fe28531a5ba0d8d042b45ec4eebe5331fad35697
Andrew Severin <severin@iastate.edu>
2016-05-24 10:26:50 -0500

    deleted example used to correct parameters

--- runBEAGLE_PhaseImpute.sh
+++ runBEAGLE_PhaseImpute.sh
@@ -9,12 +9,11 @@ INPUT="$1"
 OUTPUT=$(basename ${INPUT%.*})
 
 awk '(/#/ || ($4!~"," && $5!~"," && length($4)==1 && length($5)==1))' ${INPUT} > ${OUTPUT}_biAllelic.vcf
-module use /data021/GIF/software/modules
+module use /shared/software/GIF/modules
 #module load java/1.7.0_76
 module load java/1.8.0_25-b17
 module load beagle
 
-java -jar $beagle nthreads=16 window=500000 ibd=true gtgl=psojae_47pop.sort.biallelic.vcf out=psojae_47pop.sort.biallelic_phasedImputed
 
 java -Xmx100g -jar ${beagle} \
  nthreads=16 \
