0c8377b368a82b97ed06bed97c349d51363e7fd2
Andrew Severin <severin@iastate.edu>
2016-09-16 11:44:48 -0500

    added help and check to make sure you have at least 1 parameter

--- Quiver_02.sh
+++ Quiver_02.sh
@@ -7,6 +7,31 @@
 cd $PBS_O_WORKDIR
 ulimit -s unlimited
 chmod g+rw ${PBS_JOBNAME}.[eo]${PBS_JOBID}
+
+function printUsage () {
+    cat <<EOF
+
+Synopsis
+
+    $scriptName [-h | --help] GenomeFastafile 
+
+        GenomeFastafile: Genome file from PacBio Assembly that you wish to polish with Quiver 
+
+Author
+
+    Andrew Severin, Genome Informatics Facilty, Iowa State University
+    severin@iastate.edu
+    16 December, 2016
+
+
+EOF
+}
+
+if [ $# -lt 1 ] ; then
+        printUsage
+        exit 0
+fi
+
 module use /shared/modulefiles 
 module load LAS/parallel/20150922 
 module load python/2.7.10
@@ -15,6 +40,6 @@ module load samtools
 
 cmph5tools.py merge --outFile out_all.cmp.h5 aligned_reads*
 cmph5tools.py sort --inPlace --deep out_all.cmp.h5
-
 samtools faidx $1 
 quiver out_all.cmp.h5 -j 16 -r $1 -o $1.polished
+rm align_reads*
