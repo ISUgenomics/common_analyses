1063e9d622170985f4d1c531ade73b30179a73ed
Arun Seetharam <arnstrm@iastate.edu>
2017-02-22 09:12:58 -0600

    fixed path for the program

--- runGuidenceAA.sh
+++ runGuidenceAA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-module load guidance/2.02
+module load GIF/guidance/2.02
 # proteins using MAFFT
-perl /shared/software/GIF/programs/guidance/2.02/www/Guidance/guidance.pl \
+perl perl  /work/GIF/software/programs/guidence/2.02/www/Guidance/guidance.pl \
         --seqFile "$1"     \
         --msaProgram MAFFT \
         --seqType aa    \
