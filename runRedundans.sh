ba389de59b4b32aaf8eea0294cc98753b9cd72d2
Arun Seetharam <arnstrm@iastate.edu>
2017-10-08 11:06:40 -0500

    redundans pipeline, easy and fastway to run!

---
+++ runRedundans.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# -lt 2 ] ; then
+        echo ""
+        echo "usage: ./runRedundans.sh genome.fasta *.fastq.gz"
+        echo "runs the redundans pipeline using the genome and all fastq files used for the genome assembly"
+        echo "Note: you can softlink all reads (MP and PE) in the directory and supply them as *.fastq.gz for redundans"
+        echo "needs reconfiguration if running using the long reads (pacbio or nanopore)"
+        echo ""
+        exit 0
+fi
+
+module load GIF/redundans
+genome="$1"
+shift
+fastq="$@"
+out="${genome%.*}_redundans_output"
+mkdir -p ${out}
+
+redundans.py \
+     -v \
+     -i ${fastq} \
+     -f ${genome} \
+     -o ${out} \
+     -t 16 \
+     --log redundans_out.log
