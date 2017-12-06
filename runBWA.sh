20cbd261e0d439dbfff8b5e10452941a81016b7b
Arun Seetharam <arnstrm@iastate.edu>
2017-12-06 12:16:17 -0600

    added commands to collect summary metrics after alignment

--- runBWA.sh
+++ runBWA.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 module load bwa
 module load samtools
+module load picard
+module load R
+
 GENOMEDIR="/work/GIF/arnstrm/GENOMEDB"
-#GNAME="Btau_4.6.1_chromosomes.fa"
+#GNAME="Mus_musculus.GRCm38.dna.toplevel.fa"
 GNAME="$1"
 REF="$GENOMEDIR/$GNAME"
 THREADS="16"
@@ -11,4 +14,6 @@ READ2="$3"
 SAM=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
 bwa mem -M -t ${THREADS} ${REF} ${READ1} ${READ2} > ${SAM}.sam
 samtools view --threads 16 -b -o ${SAM}.bam ${SAM}.sam
-samtools sort -m 8G -o ${SAM}_sorted.bam -T ${SAM}_temp --threads 16 ${SAM}.bam
+samtools sort -m 5G -o ${SAM}_sorted.bam -T ${SAM}_temp --threads 16 ${SAM}.bam
+picard CollectInsertSizeMetrics I=${SAM}_bt_sorted.bam O=${SAM}_insert_size_metrics.txt H=${SAM}_insert_size_histogram.pdf M=0.5
+picard CollectAlignmentSummaryMetrics  REFERENCE_SEQUENCE=${REF}  INPUT=${SAM}_bt_sorted.bam OUTPUT=${SAM}_alignment.stats
--- runBowtie2.sh
+++ runBowtie2.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 module load bowtie2
 module load samtools
+module load picard
+module load R
 GENOMEDIR="/work/GIF/arnstrm/GENOMEDB"
 GNAME="$1"
-REF="$GENOMEDIR/$GNAME"
+REF="$GENOMEDIR/${GNAME%.*}"
 THREADS="16"
 READ1="$2"
 READ2="$3"
+MAXINSERT="300"
 SAM=$(basename ${READ1%.*} | cut -f 1-2 -d "_")
-bowtie2 --end-to-end --threads ${THREADS} -x ${REF} -1 ${READ1} -2 ${READ2} -S ${SAM}_bt.sam
+bowtie2 --end-to-end --maxins ${MAXINSERT} --threads ${THREADS} -x ${REF} -1 ${READ1} -2 ${READ2} -S ${SAM}_bt.sam
 samtools view --threads 16 -b -o ${SAM}_bt.bam ${SAM}_bt.sam
 samtools sort -m 5G -o ${SAM}_bt_sorted.bam -T ${SAM}_bt_temp --threads 16 ${SAM}_bt.bam
+
+picard CollectInsertSizeMetrics I=${SAM}_bt_sorted.bam O=${SAM}_insert_size_metrics.txt H=${SAM}_insert_size_histogram.pdf M=0.5
+picard CollectAlignmentSummaryMetrics  REFERENCE_SEQUENCE=${REF}  INPUT=${SAM}_bt_sorted.bam OUTPUT=${SAM}_alignment.stats
