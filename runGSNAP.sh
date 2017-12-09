34f163268b8f112a141dba4a7aac49e8534c1c9d
Arun Seetharam <arnstrm@iastate.edu>
2017-12-09 12:11:28 -0600

    runGSNAP.sh fixed

--- runGSNAP.sh
+++ runGSNAP.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+module load gsnap
 GMAPDB="/work/GIF/arnstrm/GENOMEDB"
 DB_NAME="$1"
 R1="$2"
 R2="$3"
-OUTFILE=$(basename ${R1} |cut -f 1 -d "_")
+INSERT_SIZE="300"
+INSERT_SD="200"
+OUTFILE=$(basename ${R1} |cut -f 1,2 -d "_")
 gsnap \
    --db=${DB_NAME%.*} \
    --dir=${GMAPDB} \
@@ -11,15 +14,29 @@ gsnap \
    --batch=5 \
    --orientation=FR \
    --gunzip \
-   --pairexpect=600 \
-   --pairdev=500 \
+   --pairexpect=${INSERT_SIZE} \
+   --pairdev=${INSERT_SD} \
    --input-buffer-size=10000000 \
    --output-buffer-size=10000000 \
    --format=sam \
    --split-output=${OUTFILE}.split \
    --failed-input=${OUTFILE}.failed \
      ${R1} ${R2}
-
-
+##### OTHE OPTIONS THAT MIGHT BE RELAVANT ######
 #   --novelsplicing=1 \
 #   --max-mismatches=5 \
+################################################
+
+SAMS="${OUTFILE}.split"
+REF="{GMAPDB}/{DB_NAME}"
+module load samtools
+module load R
+module load picard
+module load java
+
+for SAM in ${SAMS}*; do
+SAM_to_sortedBAM.sh ${SAM};
+done
+samtools merge ${SAMS}_gsnap_combined.bam *_sorted.bam
+unset DISPLAY
+runAlignmentStats.sh ${REF} ${SAMS}_gsnap_combined.bam
